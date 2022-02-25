60e949df6540f2bfe9c8555ddd127c497585e511
Firman Ismail <akayasha99@gmail.com>
2022-02-25 19:54:44 +0700

    tugas 7 react js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import {Badge, Breadcrumb, Button,ButtonGroup, Card, Alert,Col,Row,Container,CardGroup} from 'react-bootstrap';
+import { FaRegUserCircle } from 'react-icons/fa';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render(){
+    return(
+      <div>
+        <Row style={{marginLeft :'30'},{marginTop:'30px'}}>
+          <Col></Col>
+          <Col >
+          <Alert >
+            Website React Bootstrap
+          </Alert>
+          </Col>
+          <Col></Col>
+          <Col sm ={3}></Col>
+          <Col md="auto">
+            <ButtonGroup >
+              <Button variant = 'primary' >Notifications <Badge bg="light" text='dark'>9</Badge></Button><br/>
+              <Button variant = 'primary'>Messages <Badge bg="light" text='dark'>19</Badge></Button>
+            </ButtonGroup>
+          </Col>
+          <Col xs lg="2">
+            <h3><FaRegUserCircle /> Andreas </h3>
+          </Col>
+        </Row>
+        <Row>
+            <Col lg={6}></Col>
+            <Col></Col>
+            <Col sm={3}>
+            <Alert  variant = 'secondary'>
+              <Breadcrumb>
+              <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+              <Breadcrumb.Item href="#">Berita</Breadcrumb.Item>
+              <Breadcrumb.Item href="#">Bola</Breadcrumb.Item>
+              </Breadcrumb>
+            </Alert>
+            </Col>
+          </Row>
+        <Container style={{marginTop : '30px'}}>
+            <CardGroup style = {{ marginLeft :'40'}}>
+            <Card >
+              <Card.Img variant='top' src='https://images.performgroup.com/di/library/GOAL/f7/ad/diego-godin-vs-thailand_l9viaa2o7afh19gjl6vnekbgq.jpg?t=-1214473334&w=1920&h=1362&quality=100' />
+                <Card.Body>
+                <Card.Title>Diego Godin Ukir Rekor Spesial Lawan Thailand</Card.Title>
+                <Card.Text>
+                  Godin sekarang menjadi pemain Uruguay dengan Caps terbanyak setelah membantu negaranya melibas Thailand di final Piala Tiongkok.
+                  Diego Godin Boleh berbangga setelah resmi menjadi ...
+                </Card.Text>
+                <Button variant='primary'>Read More</Button>
+                </Card.Body>
+              </Card>
+            <Card>
+              <Card.Img variant='top' src='https://images.performgroup.com/di/library/GOAL/30/f0/neymar-psg-paris-saint-germain-2018-19_gmakk5891n9c19udcwofmrsgc.jpg?t=-1258742791&quality=60&w=1600' />
+                <Card.Body>
+                <Card.Title>Neymar Bahas Kontrak Baru Di Paris Saint-Germain</Card.Title>
+                <Card.Text>
+                  Ayah sang pemain bintang Brazil berusaha meredam rumor ketertarikan Madrid.Neymar sedang berdiskusi
+                  dengan Paris Saint-Germain soal perpanjangan kontrak Ligue 1 Prancis,demikian kata ayahnya...
+                </Card.Text>
+                <Button variant='primary'>Read More</Button>
+                </Card.Body>
+              </Card>
+            <Card>
+              <Card.Img variant='top' src='https://images.performgroup.com/di/library/GOAL/f3/33/england-celebrate-vs-montenegro_e97kv5anpqlq1gr9nc2qaz4ko.jpg?t=-1232521935&quality=60&w=1600' />
+                <Card.Body>
+                <Card.Title>Inggris Mau Jadi Tim Terbaik Dunia</Card.Title>
+                <Card.Text>
+                 Kumpulan talenta terbaik yang dimiliki inggris saat ini membuat Barkley optimis.
+                 Ross Barkley merasa inggris sudah berada di jalur yang benar untuk bisa menjadi tim terbaik di dunia
+                 dengan banyaknya talenta yang muncul sekarang ini...
+                </Card.Text>
+                <Button variant='primary'>Read More</Button>
+                </Card.Body>
+              </Card>
+            <Card>
+              <Card.Img variant='top' src='https://images.performgroup.com/di/library/GOAL/63/7f/sani-rizki-fauzi-indonesia-u-22_4yztc4nehkt61fa7omuyagf71.jpg?t=351764441&quality=60&w=1600' />
+                <Card.Body>
+                <Card.Title>Sani Rizki Fauzi Tegaskan Mental Pemain Timnas Indonesia U-23 Tetap Bagus</Card.Title>
+                <Card.Text>
+                  Gelandang tim nasional (timnas) Indonesia U-23 Sani Rizki Fauzi,menegaskan kesiapannya untuk bermain pada laga melawaan
+                  Brunei Darusallam U-23,kedua tim akan bentrok pada laga pamungkas grup K
+                </Card.Text>
+                <Button variant='primary'>Read More</Button>
+                </Card.Body>
+              </Card>
+            </CardGroup>
+      </Container>
+      </div>
+    )
+  }
 }
 
 export default App;
